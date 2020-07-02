9946ef20a6ff5badb7e8ecfe644db593be44e600
Alistair Johnson <alistairewj@gmail.com>
2020-07-02 18:27:26 -0400

    fix regex used to convert queries for mimic-iv use

--- sql/mimic-iv/generate_tables.sh
+++ sql/mimic-iv/generate_tables.sh
@@ -1,14 +1,14 @@
 # set the project
 # gcloud config set project bidmc-covid-19
 
-export TARGET_DATASET=mimic_covid
+export TARGET_DATASET=mimic_derived
 
 # run the MIMIC-COVID queries replacing the dataset with MIMIC-IV datasets
 export COVID_PATH='../mimic-covid'
-export DATASET_REGEX='s/mimic_covid_(.+)_phi/mimic_\1/g'
+export DATASET_REGEX='s/mimic_covid_(.+)/mimic_\1/g'
 
 # generate tables in pivoted subfolder
-for d in measurement;
+for d in hospitalstay measurement;
 do
     for fn in `ls ${COVID_PATH}/$d`;
     do
@@ -23,4 +23,4 @@ do
 done
 
 # vasopressor medications also have a specific order
-cat medication/vasopressor.sql | sed -E $DATASET_REGEX | bq query --use_legacy_sql=False --replace --destination_table=${TARGET_DATASET}.vasopressor
+# cat medication/vasopressor.sql | sed -E $DATASET_REGEX | bq query --use_legacy_sql=False --replace --destination_table=${TARGET_DATASET}.vasopressor
